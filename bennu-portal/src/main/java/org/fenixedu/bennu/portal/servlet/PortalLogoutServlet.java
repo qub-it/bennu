d43cd55988eb77d97eb4fb576cbf4d4cb9d7842a
Francisco Esteves <francisco.esteves@qub-it.com>
2026-05-14 14:21:38 +0100

    Makes PortalLogoutServlet not redirect when receiving saml package Relates with: #qubIT-Omnis-11076

--- bennu-portal/src/main/java/org/fenixedu/bennu/portal/servlet/PortalLogoutServlet.java
+++ bennu-portal/src/main/java/org/fenixedu/bennu/portal/servlet/PortalLogoutServlet.java
@@ -8,24 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.portal.BennuPortalConfiguration;
 
-import com.google.common.base.Strings;
-
 /**
  * Specialized servlet that logs the current user out.
- * 
+ *
  * If CAS is enabled, the user is redirected to the CAS logout page, after
  * being logged out locally.
- * 
+ *
  * If CAS is not enabled, the user is redirected to the application's root.
- * 
+ *
  * @author João Carvalho (joao.pedro.carvalho@tecnico.ulisboa.pt)
  *
  */
 @WebServlet({ "/logout", "/logout/" })
 public class PortalLogoutServlet extends HttpServlet {
+
+    private static final String SAML_RESPONSE = "SAMLResponse";
+    private static final String SAML_REQUEST = "SAMLRequest";
 
     @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, HttpServletResponse response)
@@ -36,11 +38,14 @@ public class PortalLogoutServlet extends HttpServlet {
         // Logout locally
         Authenticate.logout(req, resp);
 
-        if (Strings.isNullOrEmpty(BennuPortalConfiguration.getConfiguration().logoutURL())) {
-            resp.sendRedirect(req.getContextPath() + "/");
-        } else {
-            resp.sendRedirect(BennuPortalConfiguration.getConfiguration().logoutURL());
+        if (!hasSamlPackage(request)) {
+            resp.sendRedirect(StringUtils.defaultIfBlank(BennuPortalConfiguration.getConfiguration().logoutURL(),
+                    req.getContextPath() + "/"));
         }
+    }
+
+    private static boolean hasSamlPackage(final HttpServletRequest request) {
+        return request.getParameter(SAML_RESPONSE) != null || request.getParameter(SAML_REQUEST) != null;
     }
 
     @Override
